c8b8a2a120607cf580124864a159b84fb51119fa
JanWerth <jan9901@hotmail.de>
2024-05-15 14:00:11 +0200

    Refactoring

---
+++ src/analysis/analyse_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def analyse_data() -> None:
+    df_assistance = pd.read_csv('data/interim/assistance.csv')
+    df_workshop = pd.read_csv('data/interim/workshop.csv')
+
+    pass

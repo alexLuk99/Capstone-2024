a776b159e82fcf35a12b2dc6147a03ac62b73129
JanWerth <jan9901@hotmail.de>
2024-05-17 18:12:52 +0200

    update gitignore

--- src/analysis/analyse_data.py
+++ src/analysis/analyse_data.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import pandas as pd
 import altair as alt
+import geopandas as gpd
+
 
 def analyse_data() -> None:
     df_assistance = pd.read_csv('data/interim/assistance.csv')
@@ -9,6 +11,27 @@ def analyse_data() -> None:
     # Create output dir
     output_path = Path('output')
     output_path.mkdir(exist_ok=True, parents=True)
+
+    # Load GeoJSON data for Europe
+    url = 'https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson'
+    geojson = gpd.read_file(url)
+
+    geo_df = pd.DataFrame(geojson)
+    count_country_of_origin = \
+    df_assistance[['Country Of Origin', 'VIN']].groupby(by='Country Of Origin', as_index=False)['VIN'].count()
+    geo_df = geo_df.merge(count_country_of_origin, left_on='ISO2', right_on='Country Of Origin', how='left')
+    geo_df['VIN'] = geo_df['VIN'].fillna(0)
+
+    map = alt.Chart(alt.Data(values=geo_df.to_dict(orient='records'))).mark_geoshape(
+        color='Country Of Origin:Q'
+    ).transform_lookup(
+        lookup='ISO2',
+        from_=alt.LookupData(count_country_of_origin, 'Country Of Origin', list(count_country_of_origin.columns))
+    ).project(
+        type='mercator'
+    )
+
+    map.save('output/map.html')
 
     # Example 1
     country_of_orgigin = alt.Chart(df_assistance).mark_bar().encode(

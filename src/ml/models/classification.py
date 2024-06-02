849b059e3a5b1c28e196d5154cef1439876e24c1
KingOfConvenience <149243213+KingOfConvenience@users.noreply.github.com>
2024-06-02 16:22:08 +0200

    Verbessertes Classificationmodel

--- src/ml/models/classification.py
+++ src/ml/models/classification.py
@@ -1,29 +1,22 @@
 import pandas as pd
+import xgboost as xgb
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.impute import SimpleImputer
-import xgboost as xgb
 import matplotlib.pyplot as plt
 
 def classification(df_assistance: pd.DataFrame, df_workshop: pd.DataFrame, save_path: str) -> None:
-    # 'VIN' als Index setzen
     df_assistance.set_index('VIN', inplace=True)
-
-    # Gruppieren und Zählen der Reparaturen pro 'VIN'
     df_repairs = df_workshop.groupby('VIN')['Q-Line'].count().rename('Anzahl Q-Line')
-
-    # Merge der Reparaturen in df_assistance
     df_assistance = df_assistance.merge(df_repairs, left_index=True, right_index=True, how='left')
-    df_assistance['Anzahl Q-Line'].fillna(0, inplace=True)  # Fehlende Werte auffüllen
+    df_assistance['Anzahl Q-Line'].fillna(0, inplace=True)
 
-    # Features und Labels trennen
     X = df_assistance[['Country Of Incident', 'Handling Call Center', 'Baureihe', 'Component', 'Outcome Description', 'RSA Successful', 'Anzahl Q-Line']]
     y = df_assistance['Merged']
 
-    # Definieren der Transformer für kategorische und numerische Daten
     categorical_columns = ['Country Of Incident', 'Handling Call Center', 'Baureihe', 'Component', 'Outcome Description', 'RSA Successful']
     numerical_columns = ['Anzahl Q-Line']
 
@@ -37,56 +30,42 @@ def classification(df_assistance: pd.DataFrame, df_workshop: pd.DataFrame, save_
         ('scaler', StandardScaler())
     ])
 
-    # ColumnTransformer zur Anwendung der jeweiligen Transformer auf die entsprechenden Spalten
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('cat', categorical_transformer, categorical_columns),
-            ('num', numerical_transformer, numerical_columns)
-        ])
-
-    # Pipeline für den gesamten Prozess
-    clf = Pipeline(steps=[
-        ('preprocessor', preprocessor),
-        ('classifier', xgb.XGBClassifier(random_state=42))
+    preprocessor = ColumnTransformer(transformers=[
+        ('cat', categorical_transformer, categorical_columns),
+        ('num', numerical_transformer, numerical_columns)
     ])
 
-    # Hyperparameter-Raster
+    clf = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('classifier', xgb.XGBClassifier(random_state=42, use_label_encoder=False))
+    ])
+
     param_grid = {
-        'classifier__n_estimators': [50, 100, 150],
-        'classifier__learning_rate': [0.01, 0.1, 0.2],
+        'classifier__n_estimators': [100, 200],
+        'classifier__learning_rate': [0.01, 0.05, 0.1],
         'classifier__max_depth': [3, 5, 7],
-        'classifier__subsample': [0.6, 0.8, 1.0],
-        'classifier__colsample_bytree': [0.6, 0.8, 1.0]
+        'classifier__subsample': [0.7, 0.8, 0.9],
+        'classifier__colsample_bytree': [0.7, 0.8, 0.9],
+        'classifier__gamma': [0, 0.1, 0.5],
+        'classifier__min_child_weight': [1, 5, 10]
     }
 
-    # GridSearchCV Initialisieren
     grid_search = GridSearchCV(clf, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
-
-    # Aufteilen in Trainings- und Testdatensätze
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-    # Hyperparameter-Tuning und Modell-Training
     grid_search.fit(X_train, y_train)
 
-    # Beste Parameter und bestes Modell
     best_params = grid_search.best_params_
     best_model = grid_search.best_estimator_
 
     print(f"Beste Parameter: {best_params}")
-
-    # Vorhersagen treffen
     y_pred = best_model.predict(X_test)
-
-    # Modellbewertung
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
 
-    # ROC-Kurve und AUC berechnen
     y_pred_proba = best_model.predict_proba(X_test)[:, 1]
     fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
     roc_auc = auc(fpr, tpr)
 
-    # ROC-Kurve plotten und speichern
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
@@ -99,15 +78,9 @@ def classification(df_assistance: pd.DataFrame, df_workshop: pd.DataFrame, save_
     plt.savefig(save_path)
     plt.show()
 
-    # Feature-Wichtigkeit anzeigen
     model = best_model.named_steps['classifier']
     feature_importances = model.feature_importances_
-
-    # Holen der Feature-Namen
-    categorical_features = best_model.named_steps['preprocessor'].named_transformers_['cat'][
-        'onehot'].get_feature_names_out(categorical_columns)
-    numerical_features = numerical_columns  # numerische Features haben keine Transformation
-    all_features = list(categorical_features) + numerical_features
-
+    categorical_features = best_model.named_steps['preprocessor'].named_transformers_['cat']['onehot'].get_feature_names_out(categorical_columns)
+    all_features = list(categorical_features) + numerical_columns
     importances = pd.Series(feature_importances, index=all_features)
-    print(importances.sort_values(ascending=False))
+    print(importances.sort_values)
